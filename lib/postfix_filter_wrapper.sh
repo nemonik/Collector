7ecfe5761880f895532455ce9d26081a8e0f7622
Michael Walsh <github.com@nemonik.com>
2009-08-24 20:21:01 +0000

    not quite 3-line wrapper postfix filter to call the daemon...

---
+++ lib/postfix_filter_wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Simple shell-based wrapper for the POSTFIX URL Daemon filter. Invoked as
+# follows:
+#
+# /path/to/script -f sender recipients...
+
+trap "rm -f in.$$" 0 1 2 3 15
+cd /var/spool/mail/filter && cat >in.$$
+/usr/sbin/sendmail.postfix -G -i "$@" <in.$$
+/usr/bin/nc -w 30 localhost 8081 <in.$$
+exit $?
