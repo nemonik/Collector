1cbd191b9894c49e624a81dc935a2bc5916b26e9
Michael Walsh <github.com@nemonik.com>
2009-12-01 21:19:50 +0000

    moved distributable to executable

--- OOoConversionSrvc/bin/mvn-build.sh
+++ OOoConversionSrvc/bin/mvn-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ..
 mvn -e -D maven.test.skip=true clean compile package pre-site install
-cp ./target/OOoConversionSrvc-1.0-SNAPSHOT-executable.jar ./distributable/.
+cp ./target/OOoConversionSrvc-1.0-SNAPSHOT-executable.jar ./executable/.
 cd ./bin
